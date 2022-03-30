6390e49a29dfe9e23896e721b994e26ec5d5db7f
ChengChen <cchen80@stevens.edu>
2022-03-29 21:51:39 -0400

    add seed.js and update mongoConnection.js

--- server/data/verify.js
+++ server/data/verify.js
@@ -15,10 +15,10 @@ function checkEmail(str) {
     if (!str) throw 'Email must be provided';
     if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, '')) throw 'Email cannot have spaces';
     //check format XX@XX.com
-    if (/([a-zA-Z0-9]+)([\_\.\-{1}])?([a-zA-Z0-9]+)\@([a-zA-Z0-9]+)([\.])com/.test(email.trim()) == false)
+    if (/([a-zA-Z0-9]+)([\_\.\-{1}])?([a-zA-Z0-9]+)\@([a-zA-Z0-9]+)([\.])com/.test(str.trim()) == false)
         throw 'You must provide a valid email address \n - Start and end with an alphanumeric character \n - Can contain an underscores (_), dashes (-), or periods (.) \n - Domain name follows after at symbol (@) \n - Domain name is any alphanermic character(s) followed by .com';
-    email = email.trim().split('@');
-    if (email[0].length < 6 || email[1].length < 7)
+    str = str.trim().split('@');
+    if (str[0].length < 6 || str[1].length < 7)
         throw 'You must provide email prefix with at least 6 digits and domain at least 3 digits';
 }
 function checkPassword(str) {
