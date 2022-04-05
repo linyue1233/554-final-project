fb4cdbe5fcbb992c7cf728e395699f1c1eb7337d
ChengChen <cchen80@stevens.edu>
2022-04-04 23:43:13 -0400

    update users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs =require('fs');
+const fs = require('fs');
 const util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
 const router = express.Router();
@@ -9,32 +9,31 @@ const verify = require('../data/verify');
 const xss = require('xss');
 
 const multer = require('multer');
-const upload = multer({dest:'uploads/'});
-const {uploadFile,getFileStream} = require('../config/awsS3');
+const upload = multer({ dest: 'uploads/' });
+const { uploadFile, getFileStream } = require('../config/awsS3');
 
 router.get('/avatarImage/:keyId', async (req, res) => {
     const keyId = req.params.keyId;
-    try{
+    try {
         const readStream = await getFileStream(keyId);
         readStream.pipe(res);
-    }catch(e){
-        res.status(500).json({message: e});
+    } catch (e) {
+        res.status(500).json({ message: e });
     }
+});
 
-})
-
-router.post('/avatarImage', upload.single('avatar'),async (req,res) => {
+router.post('/avatarImage', upload.single('avatar'), async (req, res) => {
     const file = req.file;
     // reset filename
     file.filename = `${Date.now()}-${req.file.originalname}`;
-    console.log(file)
-    try{
+    console.log(file);
+    try {
         const result = await uploadFile(file);
         // delete local record
         fs.unlinkSync(file.path);
-        res.send({imagePath: `/avatarImage/${result.key}`});
-    }catch (error) {
-        res.status(500).json({message: error});
+        res.send({ imagePath: `/avatarImage/${result.key}` });
+    } catch (error) {
+        res.status(500).json({ message: error });
     }
 });
 
@@ -52,13 +51,16 @@ router.post('/signup', async (req, res) => {
     let userInfo = req.body;
     // check format
     try {
-        if (!userInfo || !userInfo.username || !userInfo.password || !userInfo.email) throw `lost user information`;
+        if (!userInfo || !userInfo.username || !userInfo.password || !userInfo.email || !userInfo.avatar)
+            throw `lost user information`;
         verify.isString(userInfo.username, 'username');
         verify.isString(userInfo.email, 'email');
         verify.isString(userInfo.password, 'password');
+        verify.isString(userInfo.avatar, 'avatar');
         verify.checkUsername(userInfo.username);
         verify.checkEmail(userInfo.email);
         verify.checkPassword(userInfo.password);
+        verify.checkSpace(userInfo.avatar, 'avatar');
     } catch (error) {
         res.status(400).json({ message: error });
         return;
@@ -68,7 +70,8 @@ router.post('/signup', async (req, res) => {
         const newUser = await userData.createUser(
             xss(req.body.username).trim(),
             xss(req.body.email).toLowerCase().trim(),
-            xss(req.body.password).trim()
+            xss(req.body.password).trim(),
+            xss(req.body.avatar).trim()
         );
         res.status(200).json(newUser);
     } catch (error) {
@@ -122,36 +125,36 @@ router.get('/:userId', async (req, res) => {
     }
 });
 // remove user by ID
-router.delete('/delete/:userId', async (req, res) =>{
+router.delete('/delete/:userId', async (req, res) => {
     let userId = req.params.userId.trim();
     // check format
     try {
-        verify.isString(userId, 'User ID')
-        verify.checkSpace(userId, 'User ID')
+        verify.isString(userId, 'User ID');
+        verify.checkSpace(userId, 'User ID');
     } catch (error) {
         res.status(400).json({ message: error });
         return;
     }
     // delete user
     try {
-        const deleteUser = await userData.deleteUserById(userId)
+        const deleteUser = await userData.deleteUserById(userId);
         res.status(200).json(deleteUser);
     } catch (error) {
         res.status(500).json({ message: error });
     }
-})
+});
 // change password
-router.post('/password/:userId', async (req, res) =>{
+router.post('/password/:userId', async (req, res) => {
     let userInfo = req.body;
     // check format
     try {
-        verify.isString(req.params.userId, "User ID")
-        verify.isString(userInfo.oldPassword, "Old Password")
-        verify.isString(userInfo.newPassword, "new Password")
-        verify.checkSpace(req.params.userId, "User ID")
-        verify.checkPassword(userInfo.oldPassword)
-        verify.checkPassword(userInfo.newPassword)
-        if(userInfo.oldPassword === userInfo.newPassword) throw `Please input a different password`
+        verify.isString(req.params.userId, 'User ID');
+        verify.isString(userInfo.oldPassword, 'Old Password');
+        verify.isString(userInfo.newPassword, 'new Password');
+        verify.checkSpace(req.params.userId, 'User ID');
+        verify.checkPassword(userInfo.oldPassword);
+        verify.checkPassword(userInfo.newPassword);
+        if (userInfo.oldPassword === userInfo.newPassword) throw `Please input a different password`;
     } catch (error) {
         res.status(400).json({ message: error });
         return;
@@ -162,15 +165,44 @@ router.post('/password/:userId', async (req, res) =>{
             req.params.userId.trim(),
             xss(req.body.oldPassword).trim(),
             xss(req.body.newPassword).trim()
-        )
-        if(updatePW){
-            res.status(200).json({ changePassword: true})
+        );
+        if (updatePW) {
+            res.status(200).json({ changePassword: true });
         }
     } catch (error) {
-        console.log(error)
-        res.status(500).json({message: error});
+        console.log(error);
+        res.status(500).json({ message: error });
     }
-})
-// TODO:update user Info
+});
+// update user Info
+router.put('/:userId', async (req, res) => {
+    let userInfo = req.body;
+    // check format
+    try {
+        verify.isString(req.params.userId, 'User ID');
+        verify.isString(userInfo.username, 'username');
+        verify.isString(userInfo.email, 'email');
+        verify.isString(userInfo.avatar, 'avatar');
+        verify.checkSpace(req.params.userId, 'User ID');
+        verify.checkUsername(userInfo.username);
+        verify.checkEmail(userInfo.email);
+        verify.checkSpace(userInfo.avatar, 'avatar');
+    } catch (error) {
+        res.status(400).json({ message: error });
+        return;
+    }
+    // update
+    try {
+        const updateUser = await userData.updateUser(
+            req.params.userId.trim(),
+            xss(req.body.username).trim(),
+            xss(req.body.email).toLowerCase().trim(),
+            xss(req.body.avatar).trim()
+        );
+        res.status(200).json(updateUser);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
 
 module.exports = router;
