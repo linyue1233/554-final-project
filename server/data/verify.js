d03ab02430a6c42568221d316c76df9c2275e9c4
ChengChen <cchen80@stevens.edu>
2022-04-16 18:15:33 -0400

    update verify

--- server/data/verify.js
+++ server/data/verify.js
@@ -7,15 +7,21 @@ function isString(str, varName) {
     if (str.trim().length == 0) throw `${varName} cannot just be empty spaces`;
 }
 function checkUsername(str) {
-    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, '')) throw 'Username cannot have spaces';
+    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, ''))
+        throw 'Username cannot have spaces';
     if (str.length < 4) throw 'Username at least 4 characters';
 }
 function checkEmail(str) {
     //check space
     if (!str) throw 'Email must be provided';
-    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, '')) throw 'Email cannot have spaces';
+    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, ''))
+        throw 'Email cannot have spaces';
     //check format XX@XX.com
-    if (/([a-zA-Z0-9]+)([\_\.\-{1}])?([a-zA-Z0-9]+)\@([a-zA-Z0-9]+)([\.])com/.test(str.trim()) == false)
+    if (
+        /([a-zA-Z0-9]+)([\_\.\-{1}])?([a-zA-Z0-9]+)\@([a-zA-Z0-9]+)([\.])com/.test(
+            str.trim()
+        ) == false
+    )
         throw 'You must provide a valid email address \n - Start and end with an alphanumeric character \n - Can contain an underscores (_), dashes (-), or periods (.) \n - Domain name follows after at symbol (@) \n - Domain name is any alphanermic character(s) followed by .com';
     str = str.trim().split('@');
     if (str[0].length < 6 || str[1].length < 7)
@@ -23,23 +29,27 @@ function checkEmail(str) {
 }
 function checkPassword(str) {
     if (!str) throw 'Password must be provided!';
-    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, '')) throw 'Password cannot have spaces';
+    if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, ''))
+        throw 'Password cannot have spaces';
     if (str.length < 6) throw 'Password must be at least 6 characters';
 }
 function checkSpace(str, varName) {
-    if (str.trim() != str.trim().replace(/\s+/g, '')) throw `${varName} cannot have spaces`;
+    if (str.trim() != str.trim().replace(/\s+/g, ''))
+        throw `${varName} cannot have spaces`;
 }
 
-function checkAvatarSuffix(avatar){
-    if (!/\.(jpg|jpeg|png|GIF|JPG|PNG)$/.test(avatar)) throw `Avatar must be the form of image`;
+function checkAvatarSuffix(avatar) {
+    if (!/\.(jpg|jpeg|png|GIF|JPG|PNG)$/.test(avatar))
+        throw `Avatar must be the form of image`;
 }
 
 let checkTags = (arr) => {
     const validTags = ['action', 'love', 'thriller', 'comedy', 'documentary']; //tbd
-    for (let tag of arr){
+    for (let tag of arr) {
         if (validTags.indexOf(tag) === -1) throw `${tag} is not a valid tag`;
     }
-}
+    if (new Set(arr).size !== arr.length) throw `Tags cannot have duplicate`;
+};
 
 module.exports = {
     isString,
