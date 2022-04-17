a3df6ed52aa7d1b9873b45b85819a4dd2300f8bf
fy-chen <fchen21@stevens.edu>
2022-04-16 23:14:02 -0400

    user page updated

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -6,6 +6,7 @@ import { Avatar } from '@mui/material';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import EmailIcon from '@mui/icons-material/Email';
 
 function TabPanel(props) {
     const { children, value, index } = props;
@@ -29,6 +30,7 @@ function User () {
     const [comments, setComments] = useState(null);
     const [error, setError] = useState(null);
     const [tabValue, setTabValue] = useState(0);
+    const [loading, setLoading] = useState(false);
     const { id } = useParams();
 
     const handleChange = (event, newValue) => {
@@ -38,50 +40,70 @@ function User () {
     useEffect(() => {
         async function fetchData() {
             try{
-                const {data} = await axios.get(`http://localhost:4000/users/${id}`);
+                setLoading(true);
+                const {data} = await axios.get(`/users/${id}`);
                 console.log(data);
                 setUserData(data);
+                setLoading(false);
             }catch(e) {
                 setError(e);
-            }
-        }
-
-        async function getLikedVideos() {
-            let likeId = userData.likeId;
-            if(likeId.length !== 0){
-                let likedVideoList = [];
-                for (let id of likeId) {
-                    const {data} = await axios.get(`http://localhost:4000/videos/${id}`);
-                    likedVideoList.push(data);
-                }
-                setLikedVideos(likedVideoList);
-            }
-        }
-
-        async function getComments() {
-            let comments = userData.commentId;
-            if(comments.length !== 0){
-                let commentList = [];
-                for (let id of comments) {
-                    const {data} = await axios.get(`http://localhost:4000/comments/${id}`);
-                    commentList.push(data);
-                }
-                setComments(commentList);
+                setLoading(false);
             }
         }
 
         fetchData();
-        getLikedVideos();
-        getComments();
+        
     }, [id]);// eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        
+        async function getLikedVideosandComments() {
+            try{
+                setLoading(true);
+                let likeId = userData.likeId;
+                if(likeId.length !== 0){
+                    let likedVideoList = [];
+                    for (let id of likeId) {
+                        const {data} = await axios.get(`/videos/${id}`);
+                        likedVideoList.push(data);
+                    }
+                    setLikedVideos(likedVideoList);
+                }
+
+                let comments = userData.commentId;
+                if(comments.length !== 0){
+                    let commentList = [];
+                    for (let id of comments) {
+                        const {data} = await axios.get(`/comments/${id}`);
+                        commentList.push(data);
+                    }
+                    setComments(commentList);
+                }
+                setLoading(false);
+            }catch (e){
+                setError(e);
+                setLoading(false);
+            }
+            
+        }
+
+
+        if(userData){
+            getLikedVideosandComments();
+        }
+
+    }, [userData]);// eslint-disable-line react-hooks/exhaustive-deps
 
     if (error) {
         return <div>{error}</div>
     } else if (userData) {
         return( 
         <div>
-            <Avatar alt={userData.name} src={userData.avatar} />
-            <p>{userData.name}</p>
+            <Avatar alt={userData.username} src={userData.avatar} />
+            <p>{userData.username}</p>
+            <br/>
+            <EmailIcon />
+            <p>{userData.email}</p>
             <Box sx={{ width: '100%' }}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <Tabs value={tabValue} onChange={handleChange}>
@@ -90,17 +112,19 @@ function User () {
                     </Tabs>
                 </Box>
                 <TabPanel value={tabValue} index={0}>
-                    {likedVideos && likedVideos.map((video) => {
+                    {likedVideos ? likedVideos.map((video) => {
                         return <p>{video.videoName}</p>
-                    })}
+                    }) : <p>No Likes Now</p>}
                 </TabPanel>
                 <TabPanel value={tabValue} index={1}>
-                    {comments.map((comment) => {
+                    {comments ? comments.map((comment) => {
                         return <p>{comment.content}</p>
-                    })}
+                    }): <p>No Comments Now</p>}
                 </TabPanel>
             </Box>
         </div>);
+    }else if(loading){
+        return <p>loading</p>
     }
     
 }
