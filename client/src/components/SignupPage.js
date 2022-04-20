d70f506e0a09a03795b1e186f9424b45f54d8bc1
OukanXu <90854608+OukanXu@users.noreply.github.com>
2022-04-20 16:50:58 -0400

    change signup form

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -13,16 +13,16 @@ function SignupPage(){
 
 
     const handlefile = async (e)=>{
-        /*const formData = new FormData();
-        console.log(e.target.files[0]);
+        const formData = new FormData();
+        //console.log(e.target.files[0]);
         formData.append("avatar", e.target.files[0] );
         newAvatar = await axios.post('/users/avatarImage',formData);
-        console.log(e.target.files[0]);
-        console.log(newAvatar); */
+        
+        newAvatar = newAvatar.data.imagePath;
         //const formData = new FormData();
         //formData.append("avatar", e.target.files[0] );
         
-        newAvatar = "https://benchmoon-554.s3.amazonaws.com/1649987446046-WechatIMG915.jpeg";
+        //newAvatar = "https://benchmoon-554.s3.amazonaws.com/1649987446046-WechatIMG915.jpeg";
     }
     
     const handleSubmit = async(e) =>{
