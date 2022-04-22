e2bbd60786be8993e92434e8f5fa527da9e6db90
linyue1233 <1191609469@qq.com>
2022-04-22 15:37:47 -0400

    add video player media and css

--- client/src/components/videoPlay/VideoPlay.js
+++ client/src/components/videoPlay/VideoPlay.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import '../../App.css';
+import React, { useEffect, useState, useMemo } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import '../../css/VideoPlayer.css';
+import { Box, Container } from '@mui/material'
+import axios from 'axios';
 
-
-function VideoPlay (){
+function VideoPlay() {
     return (
-        <div>Some test</div>
+        <div className="App-body">
+            <div width="100%" >
+                <h2>Video Name:</h2>
+            </div>
+            <br></br>
+            <div className="video-player ">
+                <ReactPlayer width='1024px' height='480px' controls url="https://benchmoon-554.s3.amazonaws.com/1649962183358-1649960458200107.mp4">
+
+                </ReactPlayer>
+            </div>
+
+        </div>
     );
 }
 
