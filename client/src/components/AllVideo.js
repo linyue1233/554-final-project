77db8d1765de893dc6efd30b60548a35048569fa
Jkang1221 <jiakangphil@163.com>
2022-05-04 20:49:29 -0400

    change the style of all videos page and search video page

--- client/src/components/AllVideo.js
+++ client/src/components/AllVideo.js
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import '../App.css';
 import OneVideo from './OneVideo';
 import axios from 'axios';
-import { Grid,Box,Button,ButtonGroup,Pagination } from '@mui/material';
+import { Grid,Box,Button,ButtonGroup,Pagination,Typography } from '@mui/material';
 
 function AllVideo() {
     const { tag } = useParams();
@@ -16,9 +16,9 @@ function AllVideo() {
     useEffect(() => {
         async function fetchData() {
             const data = await axios.get(`/videos/getAllVideosByTag/${tag}/${type}`);
-            setVideoData(group(data.data,2));
-            setCurData(group(data.data,2)[0]);
-            let pageNum = (data.data.length % 2)=== 0 ? parseInt(data.data.length / 2) :parseInt( data.data.length / 2 )+ 1;
+            setVideoData(group(data.data,4));
+            setCurData(group(data.data,4)[0]);
+            let pageNum = (data.data.length % 4)=== 0 ? parseInt(data.data.length / 4) :parseInt( data.data.length / 4 )+ 1;
             setTotalPage(pageNum);
             setPage(1);
         }
@@ -41,37 +41,51 @@ function AllVideo() {
         setCurData(videoData[value - 1]);
     }
     return videoData ? (
-        <Box> 
-            <ButtonGroup variant="text" aria-label="text button group">
-                <Button
-                    onClick={() => {
-                        setTypeHandler('likeCount');
-                    }}
-                >
-                    likeCount
-                </Button>
-                <Button
-                    onClick={() => {
-                        setTypeHandler('viewCount');
-                    }}
-                >
-                    viewCount
-                </Button>
-                <Button
-                    onClick={() => {
-                        setTypeHandler('uploadDate');
-                    }}
-                >
-                    uploadDate
-                </Button>
-            </ButtonGroup>
-            <Pagination className='pagination' count={totalPage} page={+page} showFirstButton showLastButton onChange={handChangePage}/>
-            <Grid container className="allVideo" spacing={2}>
-                {curData &&
-                curData.map((video) => {
-            return <OneVideo video={video} />})}
+        <Grid sx={{ textAlign: 'center',display: 'flex'}} direction="column" alignItems="center" justifyContent="center"> 
+            <Grid>
+                <ButtonGroup variant="text" aria-label="text button group">
+                    <Typography
+                        sx={{
+                            fontSize: '1.5rem',
+                            fontWeight: 'bold',
+                        }}
+                        color="textSecondary"
+                        component="h1"
+                    >
+                        Order By : 
+                    </Typography>
+                    <Button
+                        onClick={() => {
+                            setTypeHandler('likeCount');
+                        }}
+                    >
+                        likeCount
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            setTypeHandler('viewCount');
+                        }}
+                    >
+                        viewCount
+                    </Button>   
+                    <Button
+                        onClick={() => {
+                            setTypeHandler('uploadDate');
+                        }}
+                    >
+                        uploadDate
+                    </Button>
+                </ButtonGroup>
             </Grid>
-        </Box>  
+            <br/>
+            <Grid>
+                <Pagination  className='pagination' count={totalPage} page={+page} showFirstButton showLastButton onChange={handChangePage} />
+            </Grid>
+            <br />
+            <Grid key='card' container className="allVideo" spacing={0} direction="row" alignItems="center" justifyContent="center">
+                {curData && curData.map((video) => { return <OneVideo video={video} /> })}
+            </Grid>
+        </Grid>  
     ):(
         <div className="card col">
             <p className="card-text">
