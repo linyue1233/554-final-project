5fe8d5fe6dfd2903d52dd9cadb1d3252b22a048a
Jkang1221 <jiakangphil@163.com>
2022-05-08 21:10:46 -0400

    update all video page

--- client/src/components/OneVideo.js
+++ client/src/components/OneVideo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-import { Card, CardActionArea, CardContent, CardMedia, Typography,Grid } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Grid } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 const useStyles = makeStyles({
     card: {
         maxWidth: 250,
@@ -23,8 +25,8 @@ const useStyles = makeStyles({
         flexDirection: 'row',
     },
     media: {
-        height: '100%',
-        width: '100%',
+        height: '248px',
+        width: '231.46px',
     },
     button: {
         color: '#1e8678',
@@ -37,7 +39,7 @@ function OneVideo(props) {
     const videoCover = props.video.cover;
     const videoTitle = props.video.videoName;
     const videoDescription = props.video.description;
-    const likeCount= props.video.likeCount;
+    const likeCount = props.video.likeCount;
     const viewCount = props.video.viewCount;
     const year = props.video.uploadDate.year;
     const month = props.video.uploadDate.month;
@@ -46,60 +48,48 @@ function OneVideo(props) {
     const buildCard = () => {
         return (
             // <Grid item xs={3}>
-                <Card  className={classes.card} variant="outlined" sx={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column'}}>
-                    <CardActionArea>
-                        <Link to={`/videoPlay/${videoId}`}>
-                            <CardMedia
-                                className={classes.media}
-                                component="img"
-                                image={videoCover}
-                                title={videoTitle}
-                            />
-                            <CardContent>
-                                <Typography
-                                    className={classes.titleHead}
-                                    gutterBottom
-                                    variant="h1"
-                                    sx={{
+            <Card
+                className={classes.card}
+                variant="outlined"
+                sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}
+            >
+                <CardActionArea>
+                    <Link to={`/videoPlay/${videoId}`}>
+                        <CardMedia className={classes.media} component="img" image={videoCover} title={videoTitle} />
+                        <CardContent>
+                            <Typography
+                                className={classes.titleHead}
+                                gutterBottom
+                                variant="h1"
+                                sx={{
                                     fontSize: '1.5rem',
                                     fontWeight: 'bold',
                                 }}
-                                >
-                                    {videoTitle}
-                                </Typography>
-                                {/* <Typography
+                            >
+                                {videoTitle}
+                            </Typography>
+                            {/* <Typography
                                     variant="body2"
                                     color="textSecondary"
                                     component="p"
                                 >
                                     Description: {videoDescription}
                                 </Typography> */}
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                >
-                                    LikeCount: {likeCount}
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                    gutterBottom 
-                                >
-                                    ViewCount: {viewCount}
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                >
-                                    Date: {month}/{day}/{year}
-                                </Typography>
-                            </CardContent>
-                        </Link>
-                    </CardActionArea>
-                </Card>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                <FavoriteIcon fontSize="small" />
+                                {likeCount}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                                <VisibilityIcon fontSize="small" />
+                                {viewCount}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                <DateRangeIcon fontSize="small" /> {month}/{day}/{year}
+                            </Typography>
+                        </CardContent>
+                    </Link>
+                </CardActionArea>
+            </Card>
             // </Grid>
         );
     };
